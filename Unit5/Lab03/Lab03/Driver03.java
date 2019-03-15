7affbcb5c80710a032d163f8b315cce4adbbff50
N8-Doge <nathantchennnttcc@gmail.com>
2019-03-15 09:32:30 -0700

    Working on Lab03

---
+++ Unit5/Lab03/Lab03/Driver03.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Driver03 {
+    public static void main(String[] args)
+    {
+        int[] array = {100, 101, 102, 103, 104, 105, 106, 107, 108, 109};
+        print(array);
+        scramble(array);
+        print(array);
+        sort(array);
+        print(array);
+    }
+    
+    public static void print(int[] arr){
+        for(int i:arr){
+            System.out.print(Integer.toString(i)+" ");
+        }
+        System.out.println();
+    }
+    
+    public static void scramble(int[] scramble){
+        
+    }
+    
+    public static void sort(int[] arr){
+        
+    }
+}
